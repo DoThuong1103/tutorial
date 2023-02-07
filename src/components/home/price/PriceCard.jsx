02829b1209e403cab6451474f5eee2974c74b365
Thuong_e <dothuong2k1@gmail.com>
2023-02-07 18:02:47 +0700

    deleted:    .firebase/hosting.YnVpbGQ.cache deleted:    .firebaserc deleted:    .github/workflows/firebase-hosting-merge.yml deleted:    .github/workflows/firebase-hosting-pull-request.yml deleted:    firebase.json modified:   package-lock.json modified:   package.json deleted:    public/favicon.ico deleted:    public/images/logo.html deleted:    public/images/logo_files/all.min.css deleted:    public/images/logo_files/city-1.png deleted:    public/images/logo_files/city-2.png deleted:    public/images/logo_files/city-3.png deleted:    public/images/logo_files/city-4.png deleted:    public/images/logo_files/city-5.png deleted:    public/images/logo_files/city-6.png deleted:    public/images/logo_files/h1.png deleted:    public/images/logo_files/h2.png deleted:    public/images/logo_files/h3.png deleted:    public/images/logo_files/h4.png deleted:    public/images/logo_files/h6.png deleted:    public/images/logo_files/logo-light.png deleted:    public/images/logo_files/logo.png deleted:    public/images/logo_files/main.26d7eb63.css deleted:    "public/images/logo_files/main.a7878606.js.t\341\272\243i xu\341\273\221ng" deleted:    public/images/logo_files/p-1.png deleted:    public/images/logo_files/p-2.png deleted:    public/images/logo_files/p-4.png deleted:    public/images/logo_files/p-5.png deleted:    public/images/logo_files/p-6.png deleted:    public/images/logo_files/p-7.png deleted:    public/images/logo_files/saved_resource.html deleted:    public/images/logo_files/team-1.jpg deleted:    public/images/logo_files/team-2.jpg deleted:    public/images/logo_files/team-3.jpg deleted:    public/images/logo_files/team-4.jpg deleted:    public/images/logo_files/team-5.jpg modified:   public/index.html deleted:    public/manifest.json deleted:    public/robots.txt modified:   src/App.js modified:   src/components/common/footer/Footer.jsx modified:   src/components/common/header/Header.jsx modified:   src/components/common/header/header.css modified:   src/components/data/Data.js modified:   src/components/home/price/PriceCard.jsx modified:   src/components/home/recent/RecentCard.jsx

--- src/components/home/price/PriceCard.jsx
+++ src/components/home/price/PriceCard.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { price } from "../../data/Data";
+import React from "react"
+import { price } from "../../data/Data"
 
 const PriceCard = () => {
   return (
     <>
-      <div className="content flex mtop">
+      <div className='content flex mtop'>
         {price.map((item, index) => (
-          <div className="box shadow" key={index}>
-            <div className="topbtn">
-              <button className="btn3">{item.best}</button>
+          <div className='box shadow' key={index}>
+            <div className='topbtn'>
+              <button className='btn3'>{item.best}</button>
             </div>
             <h3>{item.plan}</h3>
             <h1>
@@ -19,9 +19,9 @@ const PriceCard = () => {
 
             <ul>
               {item.list.map((val) => {
-                const { icon, text, change } = val;
+                const { icon, text, change } = val
                 return (
-                  <li key={text}>
+                  <li>
                     <label
                       style={{
                         background: change === "color" ? "#dc35451f" : "#27ae601f",
@@ -32,11 +32,11 @@ const PriceCard = () => {
                     </label>
                     <p>{text}</p>
                   </li>
-                );
+                )
               })}
             </ul>
             <button
-              className="btn5"
+              className='btn5'
               style={{
                 background: item.plan === "Standard" ? "#27ae60" : "#fff",
                 color: item.plan === "Standard" ? "#fff" : "#27ae60",
@@ -48,7 +48,7 @@ const PriceCard = () => {
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default PriceCard;
+export default PriceCard
