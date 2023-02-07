65919bbd41849aabf02df3396dbc8b9bc035799e
Thuong_e <dothuong2k1@gmail.com>
2023-02-07 17:19:45 +0700

    new file:   .firebase/hosting.YnVpbGQ.cache new file:   .firebaserc new file:   .github/workflows/firebase-hosting-merge.yml new file:   .github/workflows/firebase-hosting-pull-request.yml new file:   firebase.json modified:   package-lock.json modified:   package.json new file:   public/images/logo.html new file:   public/images/logo_files/all.min.css new file:   public/images/logo_files/city-1.png new file:   public/images/logo_files/city-2.png new file:   public/images/logo_files/city-3.png new file:   public/images/logo_files/city-4.png new file:   public/images/logo_files/city-5.png new file:   public/images/logo_files/city-6.png new file:   public/images/logo_files/h1.png new file:   public/images/logo_files/h2.png new file:   public/images/logo_files/h3.png new file:   public/images/logo_files/h4.png new file:   public/images/logo_files/h6.png new file:   public/images/logo_files/logo-light.png new file:   public/images/logo_files/logo.png new file:   public/images/logo_files/main.26d7eb63.css new file:   "public/images/logo_files/main.a7878606.js.t\341\272\243i xu\341\273\221ng" new file:   public/images/logo_files/p-1.png new file:   public/images/logo_files/p-2.png new file:   public/images/logo_files/p-4.png new file:   public/images/logo_files/p-5.png new file:   public/images/logo_files/p-6.png new file:   public/images/logo_files/p-7.png new file:   public/images/logo_files/saved_resource.html new file:   public/images/logo_files/team-1.jpg new file:   public/images/logo_files/team-2.jpg new file:   public/images/logo_files/team-3.jpg new file:   public/images/logo_files/team-4.jpg new file:   public/images/logo_files/team-5.jpg modified:   src/App.js modified:   src/components/common/footer/Footer.jsx modified:   src/components/common/header/Header.jsx modified:   src/components/data/Data.js modified:   src/components/home/price/PriceCard.jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import "./App.css"
-import Pages from "./components/pages/Pages"
+import "./App.css";
+import Pages from "./components/pages/Pages";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ6_eYCcCqhoesv2Z9rwf1YWkpATCbAK4",
+  authDomain: "web-react1.firebaseapp.com",
+  projectId: "web-react1",
+  storageBucket: "web-react1.appspot.com",
+  messagingSenderId: "103049409028",
+  appId: "1:103049409028:web:765c64973bfa7c76bc3b51",
+  measurementId: "G-BES9JZC0WR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function App() {
-  return <Pages />
+  return <Pages />;
 }
 
-export default App
+export default App;
